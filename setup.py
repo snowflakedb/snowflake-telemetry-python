02eb9805b339eaefad7821a76bd55d89421b6fa0
Tom Monk <tom.monk@snowflake.com>
2024-02-15 12:55:23 -0800

    SNOW-1042085 Remove support for Python 3.7

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: SQL",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
