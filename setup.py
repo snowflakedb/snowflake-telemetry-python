76f8ea6708921d054d6eda0beea087ef1e4e18dd
Sindy Li <sindy.li@snowflake.com>
2024-12-12 16:19:31 -0800

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Database",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
